64bcc6de01c83e1dd8726114e25b4e6929d2503a
ggcoke <ggcoke@foxmail.com>
2013-12-07 15:53:14 +0800

    更新常量

---
+++ src/com/hoyotech/ctgames/Util/StringUtils.java
@@ -0,0 +1,11 @@
+package com.hoyotech.ctgames.util;
+
+/**
+ * Created by GGCoke on 13-12-6.
+ */
+public class StringUtils {
+
+    public static boolean isEmpty(String src) {
+        return src == null || "".equals(src);
+    }
+}
