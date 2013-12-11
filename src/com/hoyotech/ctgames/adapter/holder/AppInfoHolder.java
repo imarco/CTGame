7336fb43aa88993b88548d3293b5a4681e024bf8
tianyuac <tianyuac@qq.com>
2013-12-11 15:11:46 +0800

    修改"玩应用"中的应用信息的holder和info信息

--- src/com/hoyotech/ctgames/adapter/holder/AppInfoHolder.java
+++ src/com/hoyotech/ctgames/adapter/holder/AppInfoHolder.java
@@ -1,8 +1,15 @@
 package com.hoyotech.ctgames.adapter.holder;
 
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import com.hoyotech.ctgames.R;
+import com.hoyotech.ctgames.adapter.bean.AppInfo;
+import com.hoyotech.ctgames.util.TaskState;
 
 /**
  * Created by GGCoke on 13-12-6.
@@ -18,5 +25,42 @@ public class AppInfoHolder {
 
     //应用的简介部分
     public TextView tvSummary; // 应用的简介
+
+    public AppInfo info;
+
+    /**
+     * 从界面中获取view构件holder
+     * @param convertView parentView
+     */
+    public AppInfoHolder(View convertView) {
+        this.appImageHeader = (ImageView) convertView.findViewById(R.id.app_img_head);
+        this.appName = (TextView) convertView.findViewById(R.id.app_name);
+        this.appPackageSize = (TextView) convertView.findViewById(R.id.app_package_size);
+        this.btnOptions = (Button) convertView.findViewById(R.id.btn_options);
+        this.tvPrizeCount = (TextView) convertView.findViewById(R.id.tv_prize_count);
+        this.tvLuckyBeanCount = (TextView) convertView.findViewById(R.id.tv_luckybean_count);
+        this.btnAppBonus = (Button) convertView.findViewById(R.id.btn_app_bonus);
+    }
+
+    /**
+     * 根据appinfo的信息设置viewholder中view的状态
+     * @param appInfo app 的信息
+     */
+    public void setData(Context context, AppInfo appInfo) {
+
+        this.appImageHeader.setBackgroundDrawable(appInfo.getImg());
+        this.appName.setText(appInfo.getAppName());
+        this.appPackageSize.setText(String.valueOf(appInfo.getAppSize()));
+        this.btnOptions.setText(TaskState.getTaskStateMap().get(appInfo.getState()));
+        this.tvPrizeCount.setText(String.valueOf(appInfo.getPrizeCount()));
+        this.tvLuckyBeanCount.setText(String.valueOf(appInfo.getLuckybeanCount()));
+        TaskState.setButtonView(appInfo.getState(), context, btnOptions);
+
+    }
+
+    public void setButtonState(Context context, AppInfo appInfo) {
+        btnOptions.setText(TaskState.getTaskStateMap().get(appInfo.getState()));
+        TaskState.setButtonView(appInfo.getState(), context, btnOptions);
+    }
 }
 
