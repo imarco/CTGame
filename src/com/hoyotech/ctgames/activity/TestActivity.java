fa3d2cae39f98e2983d6c7c9c44ca37f484b6e22
tianyuac <tianyuac@qq.com>
2013-12-13 20:26:31 +0800

    接上一个版本 部分未添加的文件这次添加进去

---
+++ src/com/hoyotech/ctgames/activity/TestActivity.java
@@ -0,0 +1,19 @@
+package com.hoyotech.ctgames.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.hoyotech.ctgames.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tian
+ * Date: 13-12-12
+ * Time: 下午11:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_zone);
+    }
+}
